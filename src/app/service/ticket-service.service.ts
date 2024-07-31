7d4cf7a2702813f33f6360673136b0871f84bec6
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-07-31 15:45:34 +0100

    j'ai creer la fonction get ticket

--- src/app/service/ticket-service.service.ts
+++ src/app/service/ticket-service.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Ticket} from "../model/ticket";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketServiceService {
-  //test
+  urlApi=`http://localhost:8080/user`
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getyourTicket(ticket:Ticket):Observable<Ticket>{
+    return this.http.post(`${this.urlApi}"/GetYourTicket/{id}`,ticket)
+  }
 }
