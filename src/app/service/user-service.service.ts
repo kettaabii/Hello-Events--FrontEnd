004ef014d5ea8aafe54e015094093119e50a287e
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-07-31 15:58:14 +0100

    j'ai ajouter la fonction supprime une utilisateur

--- src/app/service/user-service.service.ts
+++ src/app/service/user-service.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Ticket} from "../model/ticket";
+import {Observable} from "rxjs";
+import {User} from "../model/user";
+import {requiresInlineTypeCheckBlock} from "@angular/compiler-cli/src/ngtsc/typecheck/src/tcb_util";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+api:`http://localhost:8080`
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+
+ListUser(user:User):Observable<User> {
+  return this.http.get(`${this.api}/admin/ListUsers`)
+
+}
+DeleteUser(id:number):Observable<any>{
+  return this.http.delete(`${this.api}/deleteUser/`+id)
+}
+
+
 }
