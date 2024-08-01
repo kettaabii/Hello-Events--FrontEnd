16ca4d41fdd8d73232cb94793872264a6d3edf37
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-01 17:40:08 +0100

    la fonction get ticket

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {HeaderBodyComponent} from "./header-body/header-body.component";
 import {SearchFormComponent} from "./search-form/search-form.component";
 import {BodyCrouselComponent} from "./body-crousel/body-crousel.component";
 import {UserComponentComponent} from "./user-component/user-component.component";
+import {TicketComponentComponent} from "./ticket-component/ticket-component.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeaderBodyComponent, SearchFormComponent, BodyCrouselComponent, UserComponentComponent],
+  imports: [RouterOutlet, HeaderComponent, HeaderBodyComponent, SearchFormComponent, BodyCrouselComponent, UserComponentComponent, TicketComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/ticket-component/ticket-component.component.ts
+++ src/app/ticket-component/ticket-component.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketServiceService } from '../service/ticket-service.service';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Ticket } from '../model/ticket';
 
 @Component({
   selector: 'app-ticket-component',
   standalone: true,
-  imports: [],
   templateUrl: './ticket-component.component.html',
-  styleUrl: './ticket-component.component.css'
+  imports: [
+    ReactiveFormsModule
+  ],
+  styleUrls: ['./ticket-component.component.css']
 })
-export class TicketComponentComponent {
+export class TicketComponentComponent implements OnInit {
+  AddTicketForm: FormGroup;
 
+  constructor(private ticketService: TicketServiceService, private fb: FormBuilder) {
+    this.AddTicketForm = this.fb.group({
+      ticketId: ['', Validators.required],
+      ticketPrice: ['', Validators.required],
+      ticketDate: ['', Validators.required],
+      ticketCode: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+    // Vous pouvez mettre ici des appels supplémentaires à l'initialisation si nécessaire
+  }
+
+  saveTicket(): void {
+    if (this.AddTicketForm.valid) {
+      const ticket: Ticket = this.AddTicketForm.value;
+      this.ticketService.saveTicket(ticket).subscribe(
+        (response: any) => {
+          console.log('Ticket saved successfully', response);
+        },
+        (error: any) => {
+          console.error('Error saving ticket', error);
+        }
+      );
+    }
+  }
 }
