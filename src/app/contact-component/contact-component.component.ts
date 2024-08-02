e3c586f4a37fa1417fcfb9d7a08ae5da024a19ac
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-02 23:18:43 +0100

    amelioration de l'interface

--- src/app/contact-component/contact-component.component.ts
+++ src/app/contact-component/contact-component.component.ts
@@ -3,11 +3,12 @@ import {User} from "../model/user";
 import {UserServiceService} from "../service/user-service.service";
 import {ContactServiceService} from "../service/contact-service.service";
 import {Contact} from "../model/contact";
+import {NgFor} from "@angular/common";
 
 @Component({
   selector: 'app-contact-component',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './contact-component.component.html',
   styleUrl: './contact-component.component.css'
 })
