fa848a1791f478ef1fda365a6a0a4d3d38519b30
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-08-02 22:47:25 +0100

    j'ai creer le component Contact

---
+++ src/app/contact-component/contact-component.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../model/user";
+import {UserServiceService} from "../service/user-service.service";
+import {ContactServiceService} from "../service/contact-service.service";
+import {Contact} from "../model/contact";
+
+@Component({
+  selector: 'app-contact-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-component.component.html',
+  styleUrl: './contact-component.component.css'
+})
+export class ContactComponentComponent implements OnInit{
+
+  GetContacts: Contact[] = [];
+
+  ngOnInit(): void {
+    this.ListContacts()
+
+
+  }
+
+
+  constructor(private contactService: ContactServiceService) {
+  }
+
+
+  ListContacts(): void {
+    this.contactService.showAllMessages().subscribe((data: Contact[]) => {
+      this.GetContacts = data;
+
+    })
+  }
+
+
+}
